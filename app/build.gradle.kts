f39ca8aff51b45958768ac089dd509cd8f7cdbc1
Sivayogeith Umamaheswaran <sivayogeith@gmail.com>
2025-10-05 18:14:41 +0530

    feat: added basic humans counter with storage system

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    kotlin("plugin.serialization") version "2.2.0"
 }
 
 android {
@@ -48,7 +49,9 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.kotlinx.serialization.json)
 }
--- app/src/main/java/net/sage/coin_clicker/ui/home/HomeFragment.kt
+++ app/src/main/java/net/sage/coin_clicker/ui/home/HomeFragment.kt
@@ -1,15 +1,24 @@
 package net.sage.coin_clicker.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import net.sage.coin_clicker.R
 import net.sage.coin_clicker.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    @Serializable
+    data class Save (
+        var count: Int = 0,
+        var upgrades: Array<Int> = arrayOf(0)) {}
 
     private var _binding: FragmentHomeBinding? = null
 
@@ -17,25 +26,44 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private var save: Save = Save(0, arrayOf(0))
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        println(Json.encodeToString(save))
+
+        try {
+            save = Json.decodeFromString<Save>(context?.openFileInput("save.json")?.bufferedReader().use { it?.readText() } as String)
+        } catch (_: Exception) { save() }
+
+        val countView: TextView = binding.count
+        val clickerView: ImageView = binding.clicker
+
+        clickerView.setOnClickListener {
+            save.count++
+            countView.text = getString(R.string.count, save.count)
+            save()
         }
+
+        countView.text = getString(R.string.count, save.count)
+
         return root
     }
 
+    fun save() {
+        context?.openFileOutput("save.json", Context.MODE_PRIVATE).use {
+            it?.write(Json.encodeToString(save).toByteArray())
+        }
+    }
+
     override fun onDestroyView() {
+        save()
         super.onDestroyView()
         _binding = null
     }
